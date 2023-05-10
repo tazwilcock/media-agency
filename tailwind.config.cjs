2d4dbf14e0d678860944cd4b10f4864774d9856c
tazwilcock <twdevdesign@gmail.com>
2023-05-10 17:09:29 -0600

    started hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+   theme: {
+      screens: {
+         sm: "480px",
+         md: "768px",
+         lg: "976px",
+         xl: "1440px",
+      },
+      extend: {
+         fontFamily: {
+            notSans: ["Josefin Slab", "serif"],
+            notAlata: ["Montserrat", "sans-serif"],
+         },
+      },
+   },
+   plugins: [],
 }
